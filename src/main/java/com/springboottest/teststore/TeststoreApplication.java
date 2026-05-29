eec6220165836ebe127d74156c935a8d7f1cdb16
sangaran10 <sangaran.ramesch@gmail.com>
2026-05-29 12:36:21 +0200

    -start first springboot project

---
+++ src/main/java/com/springboottest/teststore/TeststoreApplication.java
@@ -0,0 +1,13 @@
+package com.springboottest.teststore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeststoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeststoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboottest/teststore/TeststoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottest.teststore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeststoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
