8deb18bef583ac406fd11c52d9f32870879ea570
sangaran10 <sangaran.ramesch@gmail.com>
2026-06-04 13:33:12 +0200

    -add beans and annotation to let spring manage objects and their dependencies

--- src/main/java/com/springboottest/teststore/OrderService.java
+++ src/main/java/com/springboottest/teststore/OrderService.java
@@ -1,13 +1,26 @@
 package com.springboottest.teststore;
 
+import org.springframework.stereotype.Service;
+
+// @Component: general purpose annotation
+// @Service: classes that contain buisness logic
+// @Repository: classes that interact with a database
+// @Controller: classes that handles web requests
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
+    // @Autowired (in the past necessary to tell spring to autowire this object with its dependencies)
+    // not necessary anymore if the class has a single constructor
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     public void placeOrder() {
         paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
--- src/main/java/com/springboottest/teststore/TeststoreApplication.java
+++ src/main/java/com/springboottest/teststore/TeststoreApplication.java
@@ -1,19 +1,24 @@
 package com.springboottest.teststore;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TeststoreApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(TeststoreApplication.class, args);
+		ApplicationContext context = SpringApplication.run(TeststoreApplication.class, args);
+		// Instead of manually creating objects and injecting dependencies, we let Spring take care of that
+		var orderService = context.getBean(OrderService.class);
+		orderService.placeOrder();
 
 		// Injection of Dependency
 		// PaymentService easily exchangeable between Stripe and PayPal without changing Order Service
-		var orderService = new OrderService(new PayPalPaymentService());
+		// var orderService = new OrderService(new PayPalPaymentService());
 
-		orderService.placeOrder();
-		
+		// orderService.setPaymentService(new PayPalPaymentService());
+		// Setter Injection possible for optional dependencies, mostly constructor injection used and recommended
 	}
 
 }
